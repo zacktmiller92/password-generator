8643370e9087b880e473e3a394213b16c5d77de8
zacktmiller92 <zacktmiller92@gmail.com>
2021-07-02 18:11:07 -0400

    added logic for number of characters prompt validation but there is a bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,58 @@
 // Assignment code here
 
+  var specialChars = ['!', '#', '$', '%', '&', '(', ')', '*', ',', '-', '.', '/', ':', ';', '<', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
+  var nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  var lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  var upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  
+
 function generateRandomNumber (min,max) {
   var value = Math.floor(Math.random() * (max - min + 1) + min);
   return value;
 };
 
 function generatePassword() {
-  var specialChars = ['!', '#', '$', '%', '&', '(', ')', '*', ',', '-', '.', '/', ':', ';', '<', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
-  var nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-  var lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-  var upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   var chars = []
-
+  var passwordOutput = []
+  
   var howManyCharacters = window.prompt("How many characters do you want?")
-  var howManyCharacters = parseInt(howManyCharacters)
-  console.log(howManyCharacters)
-  console.log(typeof(howManyCharacters))
+  howManyCharacters = parseInt(howManyCharacters)
+  debugger;
+  if (
+    !isNaN(howManyCharacters) &&
+    howManyCharacters <= 128 &&
+    howManyCharacters >= 8
+    ) {
+      
+  } else {
+    window.alert("Please enter a number between 8 and 128 characters.")
+    generatePassword();
+  }
+
   var wantNumbers = window.confirm("Do you want numbers?")
   var wantSpecialChars = window.confirm("Do you want special characters?")
   var wantUpper = window.confirm("Do you want uppercase characters?")
   var wantLower = window.confirm("Do you want lowercase characters?")
   
-  // if numbers, append numbers to character array
+  if (wantNumbers) {
+    chars = chars.concat(nums)
+  };
+  if (wantSpecialChars) {
+    chars = chars.concat(specialChars)
+  };
+  if (wantUpper) {
+    chars = chars.concat(upperLetters)
+  };
+  if (wantLower) {
+    chars = chars.concat(lowerLetters)
+  };
 
-  // if sc, append special characters to character array
-  // if ul, append upper case letetrs to characters array
-  // if ll, append lowercase letters
   //generate the password
-  //repeat based on number of desired characters
-  //add random character from character array to password
-  //return the password
-  // return "asdf";
+  for (var i=0; i < howManyCharacters; i++) {
+    passwordOutput = passwordOutput.concat(chars[generateRandomNumber(1,chars.length)])
+  }
+
+  return passwordOutput.join("");
 };
 
 // Get references to the #generate element
