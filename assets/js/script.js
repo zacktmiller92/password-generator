009be5c8453a060911688aeabe348fcf18f6fda0
zacktmiller92 <zacktmiller92@gmail.com>
2021-07-02 15:28:21 -0400

    updated directory structure and started writing out JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,27 @@
 // Assignment code here
 
+function generatePassword() {
+  var specialChars = ['!', '#', '$', '%', '&', '(', ')', '*', ',', '-', '.', '/', ':', ';', '<', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
+  var nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  var lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  var upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  var chars = []
+
+  var howManyCharacters = prompt("How many characters do you want?")
+  var wantNumbers = prompt("Do you want numbers?")
+  var wantSpecialChars = prompt("Do you want special characters?")
+  var wantUpper = prompt("Do you want uppercase characters?")
+  var wantLower = prompt("Do you want lowercase characters?")
+  // if numbers, append numbers to character array
+  // if sc, append special characters to character array
+  // if ul, append upper case letetrs to characters array
+  // if ll, append lowercase letters
+  //generate the password
+  //repeat based on number of desired characters
+  //add random character from character array to password
+  //return the password
+};
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
