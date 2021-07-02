6f5c0b86de0284055365e69abdfd1fe7711aed29
zacktmiller92 <zacktmiller92@gmail.com>
2021-07-02 15:57:59 -0400

    added random number generator helper function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 // Assignment code here
 
+function generateRandomNumber (min,max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+  return value;
+};
+
 function generatePassword() {
   var specialChars = ['!', '#', '$', '%', '&', '(', ')', '*', ',', '-', '.', '/', ':', ';', '<', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
   var nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -7,12 +12,17 @@ function generatePassword() {
   var upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   var chars = []
 
-  var howManyCharacters = prompt("How many characters do you want?")
-  var wantNumbers = prompt("Do you want numbers?")
-  var wantSpecialChars = prompt("Do you want special characters?")
-  var wantUpper = prompt("Do you want uppercase characters?")
-  var wantLower = prompt("Do you want lowercase characters?")
+  var howManyCharacters = window.prompt("How many characters do you want?")
+  var howManyCharacters = parseInt(howManyCharacters)
+  console.log(howManyCharacters)
+  console.log(typeof(howManyCharacters))
+  var wantNumbers = window.confirm("Do you want numbers?")
+  var wantSpecialChars = window.confirm("Do you want special characters?")
+  var wantUpper = window.confirm("Do you want uppercase characters?")
+  var wantLower = window.confirm("Do you want lowercase characters?")
+  
   // if numbers, append numbers to character array
+
   // if sc, append special characters to character array
   // if ul, append upper case letetrs to characters array
   // if ll, append lowercase letters
@@ -20,8 +30,8 @@ function generatePassword() {
   //repeat based on number of desired characters
   //add random character from character array to password
   //return the password
+  // return "asdf";
 };
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
